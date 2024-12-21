78fcb59ba4b014f5d19d9bfe00d947061f8ebb88
Andrey D Araya <andreydelgadoaraya@gmail.com>
2024-12-20 21:12:09 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://andreydAraya.github.io",
+  base: "/my_porfolio", // Add base path for GitHub Pages
 });
